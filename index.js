f2ce3821b72201c26a39b0fc58e828f6fc28b679
Endzo Useri <CoffeeBlack7@protonmail.com>
2021-08-14 16:42:37 -0700

    Update JS

--- index.js
+++ index.js
@@ -28,4 +28,16 @@ document.getElementById("usernameInput").addEventListener("keyup", function(even
     4. Log to the console to demonstrate it works
 */
 
-let searchQuery = .addEventListener("keyup").toLowerCase
+document.getElementById("searchInput").addEventListener("keyup", function(event) {
+    let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName("name");
+    console.log(allNamesDOMCollection[0].textContent);
+})
+
+/*
+    Task: Get all the name elements from the DOM
+    1. Create a new 'let' called 'allNamesDOMCollection'
+    2. Store all the elements of classname 'name' in 'allNamesDOMCollection'
+    3. Log to the console 'allNamesDOMCollection' to demonstrate that an HTMLCollection exists
+    4. Log out the first index of allNamesDOMCollection and add 'textContent' to view a name
+*/
