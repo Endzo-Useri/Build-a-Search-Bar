557b12ed3bd7b811f95c865eb1e51e9c77a2585a
Endzo Useri <CoffeeBlack7@protonmail.com>
2021-09-09 15:23:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,11 @@ document.getElementById("searchInput").addEventListener("keyup", function(event)
 
 	for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
 		const currentName = allNamesDOMCollection[counter].textContent.toLocaleLowerCase;
-		console.log(currentName);
+
+		if (currentName.includes(searchQuery)) {
+			allNamesDOMCollection[counter].style.display = block;
+		} else {
+			allNamesDOMCollection[counter].style.display = "none";
+		}
 	}
 });
