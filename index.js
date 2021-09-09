99b7e4d76aa11799af14f272ff133e15c7d5d6b1
Endzo Useri <CoffeeBlack7@protonmail.com>
2021-09-09 14:35:46 -0700

    Update index.js, index.html

--- index.js
+++ index.js
@@ -45,5 +45,9 @@ document.getElementById("usernameInput").addEventListener("keyup", function(even
 document.getElementById("searchInput").addEventListener("keyup", function(event) {
   let searchQuery = event.target.value.toLowerCase();
 	let allNamesDOMCollection = document.getElementsByClassName("name")
-	console.log(allNamesDOMCollection[0].textContent);
+
+	for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+		const currentName = allNamesDOMCollection[counter].textContent.toLocaleLowerCase;
+		console.log(currentName);
+	}
 });
